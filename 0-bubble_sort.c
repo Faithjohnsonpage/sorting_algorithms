11f852e23086a165720eeae4e0915326c2a15114
Faithjohnsonpage <ermac_4life@yahoo.com>
2024-01-21 17:52:21 +0100

    0-bubble_sort.c

---
+++ 0-bubble_sort.c
@@ -0,0 +1,40 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ * using the Bubble Sort algorithm.
+ *
+ * @array: Pointer to the first element of the array to be sorted.
+ * @size: Number of elements in the array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp, flag;
+
+	if (size == 1)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		flag = 0;
+
+		for (j = 0; j < size - 1 - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+
+			flag = 1;
+		}
+
+
+		if (flag == 0)
+			break;
+	}
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# C - Sorting algorithms & Big O
+C
+Algorithm
+Data structure
+
+* These repo covers solution to tasks in Sorting algorithms & Big O
